e993c8da29e4fb566f84a59e6faaee68bad24025
Feroz <mferoz.bscs17seecs@seecs.edu.pk>
2021-08-30 15:48:50 +0500

    add helpers methos for User and Tasks

---
+++ App/Infrastructor/utils/userUtils.ts
@@ -0,0 +1,13 @@
+import bcrypt from 'bcrypt';
+import * as jwt from 'jsonwebtoken';
+
+export const authToken = (data: object | string, secret: string): string => {
+    return jwt.sign(data,secret);
+};
+
+export const comparePasswords = async (plain:string,hashed:string):Promise<boolean>=>{
+    return await bcrypt.compare(plain,hashed);
+}
+export const decodeToken = (token: any, secret: string) : any =>{
+    return jwt.verify(token,secret)
+}
