5433d1ca410fd0be34088800e21063a78b908845
Muhammad Feroz <feroz@carbonteq.com>
2021-09-13 16:44:02 +0500

    add cors

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,17 @@
-import express, { Application, Request, Response } from 'express';
+import express, { Application } from 'express';
+import cors from "cors"
+
 import routes from "./Http/routes";
 import dbInit from "./Infrastructor/init";
 
 
 dbInit();
 const app: Application = express();
+
+app.use(cors({
+    origin: 'http://localhost:3000',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+  }))
 app.use(express.json());
 app.use('/api/v1',routes);
 
